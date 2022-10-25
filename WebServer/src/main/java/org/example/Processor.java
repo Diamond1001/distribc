6d55d418ed3c47d477797ee0ebadbe07e1d48158
Алмаз Тургинбеков <aaalll020810@gmail.com>
2022-10-25 13:32:50 +0600

    upd
    upd

--- WebServer/src/main/java/org/example/Processor.java
+++ WebServer/src/main/java/org/example/Processor.java
@@ -44,12 +44,28 @@ public class Processor {
             socket.close();
         } else if (request.getRequestLine().contains("/exec/params")){
             System.out.println("Annyong Haseyo");
+            int random = 100 + (int) (Math.random() * 10000);
+            int random1 = 1000 + (int) (Math.random() * 1000000);
+            int random2 = 10000 + (int) (Math.random() * 10000000);
+            int sum=0;
+            int sum1=0;
+            int sum2=0;
+            for (int i=0;i<random;i++){
+                sum+=i;
+            }
+            for (int i=0;i<random1;i++){
+                sum1+=i;
+            }
+            for (int i=0;i<random2;i++){
+                sum2+=i;
+            }
             output.println("HTTP/1.1 200 OK");
             output.println("Content-Type: text/html; charset=utf-8");
             output.println();
             output.println("<html>");
             output.println("<head><title>Parameter</title></head>");
             output.println("<body><p>Parameter was executed</p></body>");
+            output.println("<body><p>"+random+ " : "+ sum + " : "+ sum1 +" : "+ sum2 +"</p></body>");
             output.println("</html>");
             output.flush();
             socket.close();
